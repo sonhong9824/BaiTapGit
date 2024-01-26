2a368c31a087b5d44fce1d6ef2e58319d05075c9
sonpro9824 <sonpro9824@gmail.com>
2024-01-26 22:11:36 +0700

    new

---
+++ Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo
+{
+    internal class Program
+    {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+    }
+}
